e73c8ac9ab8dfdc7c8519b74ae674563010e6a51
Kanyshai Osmonova <kanyshai.osmonova.kk@gmail.com>
2022-09-21 11:39:15 +0600

    created React Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-undef */
+/* eslint-disable jsx-a11y/heading-has-content */
+import Card from './components/Card';
+import Header from './components/Header';
+import Drawer from './components/Drawer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper clear "> 
+    <Drawer />
+    <Header />
+    
+    <div className="content p-40">
+      <div className="d-flex justify-between align-center mb-40">
+        <h1>Все кроссовки</h1>
+        <div className="search-block d-flex">
+          <img src="/img/search.svg" alt="search svg" />
+          <input placeholder="Поиск ..."/> 
+        </div>
+      </div>
+      
+      <div className="d-flex">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+
+      </div>
+
     </div>
+    </div>
+    
   );
 }
+
 
 export default App;
