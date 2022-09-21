e35f93e49dab6ce5f731eadba860d51acf5b2505
Kanyshai Osmonova <kanyshai.osmonova.kk@gmail.com>
2022-09-21 21:31:33 +0600

    create arrays and functions for components, work with CSS modules

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-/* eslint-disable no-undef */
-/* eslint-disable jsx-a11y/heading-has-content */
+/* eslint-disable array-callback-return */
 import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
+
+const arr = [
+  {
+    title : 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price :  12999,
+    imageUrl : '/img/sneakers/sneaker1.svg',
+  },
+  {
+    title : 'Мужские Кроссовки Nike Air Max 270',
+    price : 12999,
+    imageUrl : '/img/sneakers/sneaker2.svg',
+  },
+  {
+    title : 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price : 8499,
+    imageUrl : '/img/sneakers/sneaker3.svg',
+  },
+  {
+    title : 'Кроссовки Puma X Aka Boku Future Rider',
+    price : 8999,
+    imageUrl : '/img/sneakers/sneaker4.svg',
+  },
+
+]
 
 function App() {
   return (
@@ -20,10 +43,14 @@ function App() {
       </div>
       
       <div className="d-flex">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+        {arr.map((obj)=>(
+          <Card 
+          title={obj.title}
+          price={obj.price}
+          imageUrl={obj.imageUrl}
+          onClick={()=>console.log(obj)}
+          />
+        ))}
 
       </div>
 
