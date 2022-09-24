4febf19987fcc5fc0a9b7cac36b22b24ab45d57e
Kanyshai Osmonova <kanyshai.osmonova.kk@gmail.com>
2022-09-24 15:01:52 +0600

    work with Async/Await functions and React Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import {Route,Routes} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import axios from 'axios';
-import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
+import Home from './pages/Home';
+import Favorites from './pages/Favorites';
 
 function App() {
   const[items, setItems]  = React.useState([]);
   const[favorites, setFavorites]  = React.useState([]);
   const[searchValue, setSearchValue]  = React.useState('');
   const[cartOpened, setCartOpened] = React.useState(false);
-  const[cartItems, setCartItems]  = React.useState([
-
-    {"title":"Мужские Кроссовки Nike Blazer Mid Suede",
-    "price":12999,
-    "imageUrl":"/img/sneakers/sneaker1.svg"},
-
-    {"title":"Мужские Кроссовки Nike Air Max 270",
-    "price":12999,
-    "imageUrl":"/img/sneakers/sneaker2.svg"},]);
+  const[cartItems, setCartItems]  = React.useState([]);
 
   React.useEffect(() => {
     axios.get('https://632c1b791aabd83739934541.mockapi.io/items').then(res => {
@@ -27,6 +20,9 @@ function App() {
     });
     axios.get('https://632c1b791aabd83739934541.mockapi.io/cart').then(res => {
       setCartItems(res.data);
+    });
+    axios.get('https://632c1b791aabd83739934541.mockapi.io/favorites').then(res => {
+      setFavorites(res.data);
     });
   },[]);
 
@@ -41,55 +37,43 @@ function App() {
     axios.delete(`https://632c1b791aabd83739934541.mockapi.io/cart/${id}`);
     setCartItems(prev => prev.filter(item => item.id !== id));
   }
-  const onAddToFavorite = (obj) => {
-    axios.post('https://632c1b791aabd83739934541.mockapi.io/favorites', obj);
-    setFavorites(prev => [...prev, obj]);
-  };
+  const onAddToFavorite = async(obj) => {
+    try{
+      if(favorites.find((favObj) => favObj.id === obj.id)){
+        axios.delete(`https://632c1b791aabd83739934541.mockapi.io/favorites/${obj.id}`);
+      } else {
+        const {data} = await axios.post('https://632c1b791aabd83739934541.mockapi.io/favorites', obj);
+        setFavorites(prev => [...prev, data]);
+      }
+  }
+  catch(error) {
+    alert('Не удалось добавить в фавориты')
+  }
+};
 
   return (
     <div className="wrapper clear "> 
     {cartOpened && <Drawer items={cartItems} onClose={()=>setCartOpened(false)} onRemove={onRemoveItem}/>}
-    <Routes>
-    <Route path="/favorites" exact>
       <Header onClickCart={()=>setCartOpened(true)}/>
-    </Route>
-    </Routes>
-  
 
-    <div className="content p-40">
-      <div className="d-flex justify-between align-center mb-40">
-        <h1>{searchValue ? `Поиск по запросу : ${searchValue}` : 'Все кроссовки'}</h1>
-        <div className="search-block d-flex">
-          <img src="/img/search.svg" alt="search svg" />
-          {searchValue && (
-          <img
-            onClick={() => setSearchValue('')} 
-            className="clear cu-p" 
-            src="/img/btn-remove.svg" 
-            alt="clear" />
-            )}
-          <input 
-            onChange={onChangeSearchInput} 
-            value={searchValue} 
-            placeholder="Поиск ..."/> 
-        </div>
-      </div>
-      
-      <div className="d-flex flex-wrap">
-        {items.filter(item=>item.title.toLowerCase().includes(searchValue.toLowerCase())).map((item, index)=>(
-          <Card 
-          key={index}
-          title={item.title}
-          price={item.price}
-          imageUrl={item.imageUrl}
-          onFavorite={onAddToFavorite}
-          onPlus={(obj) => {onAddToCart(obj)}}
-          />
-        ))}
+    <Route path="/" exact>
+      <Home 
+        items={items}
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+        onChangeSearchInput={onChangeSearchInput}
+        onAddToFavorite={onAddToFavorite}
+        onAddToCart={onAddToCart}
+        />
+    </Route> 
 
-      </div>
-
-    </div>
+    <Route path="/favorites" exact>
+      <Favorites
+        items={favorites} 
+        onAddToFavorite={onAddToFavorite}
+        />
+    </Route> 
+    
     </div>
     
   );
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,16 +2,16 @@
 import React from 'react';
 import styles from './Card.module.scss';
 
-function Card({title, imageUrl, price, onFavorite, onPlus}) {
+function Card({id, title, imageUrl, price, onFavorite, onPlus, favorited = false}) {
 const [isAdded, setIsAdded] = React.useState(false);
-const [isFavorite, setIsFavorite] = React.useState(false);
+const [isFavorite, setIsFavorite] = React.useState(favorited);
 
 const onClickPlus = () => {
     onPlus({title, imageUrl, price});
     setIsAdded(!isAdded);
 }
 const onClickFavorite = () => {
-    onFavorite({title, imageUrl, price});
+    onFavorite({id, title, imageUrl, price});
     setIsFavorite(!isFavorite);
 }
     return (
